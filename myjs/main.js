2aa0911b922622e3fcfe732dfcbc018799bc86b6
snowdy78 <themrleoner12@mail.ru>
2024-10-03 13:39:34 +0700

    lab-10 ready

--- myjs/main.js
+++ myjs/main.js
@@ -29,16 +29,40 @@ function validateField(className, type) {
         }
     }
 }
+function addValidationOnButtonClick() {
+    let buttons = document.querySelector('button');
+    if (buttons) {
+        //в 1 параметре - событие (щелчок по кнопке)
+        //во 2 - анонимная функция с контекстом вызвавшим событие
+        buttons.addEventListener('click', function (e) {
+            //метод, предотвращающий реакцию по умолчанию
+            e.preventDefault();
+            //Значение из поля e-mail
+            validateField('contact-email', 'email');
+            validateField('contact-name');
+        });
+    }
+}
+function switchSignMode() {
+    let sign_form = document.getElementById('sign-form');
+    let ul = document.getElementById('sign-ul');
+
+    if (sign_form.classList.contains('hidden-element')) {
+        ul.classList.add('hidden-element');
+        sign_form.classList.remove('hidden-element');
+    } else if (ul.classList.contains('hidden-element')) {
+        sign_form.classList.add('hidden-element');
+        ul.classList.remove('hidden-element');
+    }
+}
 document.addEventListener("DOMContentLoaded", () => {
     //Получаем список элементов родителя
-    let buttons = document.querySelector('button');
-    //в 1 параметре - событие (щелчок по кнопке)
-    //во 2 - анонимная функция с контекстом вызвавшим событие
-    buttons.addEventListener('click', function (e) {
-        //метод, предотвращающий реакцию по умолчанию
-        e.preventDefault();
-        //Значение из поля e-mail
-        validateField('contact-email', 'email');
-        validateField('contact-name');
-    });
+    addValidationOnButtonClick();
+
+    let sign_button = document.getElementById('sign-btn');
+
+    sign_button.addEventListener('click', switchSignMode);
+
+    let form_sign_button = document.getElementById('out-btn');
+    form_sign_button.addEventListener('click', switchSignMode);
 });
