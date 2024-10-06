ed2b346efeb8a1e2fd2409dc10e4b9da9565de04
snowdy78 <themrleoner12@mail.ru>
2024-10-06 19:26:04 +0700

    lab-12 ready

--- myjs/ajax.js
+++ myjs/ajax.js
@@ -1,50 +1,49 @@
-//сюда передаем ссылку
-function linkcontent(link) {
-    //Получаем ID контента
-    var cont = document.getElementById('nav-tabContent');
-    //Получаем ID вывода текстового предупреждения о загрузке
-    var loading = document.getElementById('loading');
-    //получить HTML-содержимое элемента в виде строки "Идет загрузка.."
-    cont.innerHTML = loading.innerHTML;
-    //Создаем объект Ajax
-    var http = createRequestObject();
-    if( http ) //если объект создан
-    {
-        //Созданный объект инициализирует новый запрос
-        //Параметры: метод GET, ссылка
-        http.open('get', link);
-        //задаётся обработчик, который вызывается при смене статуса объекта
-        http.onreadystatechange = function ()
-        {
-            //readyState — число, обозначающее статус объекта
-            //4 - объект инициализирован, получен ответ от сервера
-            if(http.readyState == 4)
-            {
-                //представление ответа сервера в виде текста
-                cont.innerHTML = http.responseText;
-            }
-        }
-        http.send(null);
-    }
-    else
-    {
-        document.location = link;
-    }
-    // создание ajax-объекта с поддержкой браузеров
-    function createRequestObject()
-    {
-        //Gecko-совместимые браузеры, Safari, Konqueror
-        try { return new XMLHttpRequest() }//создание объекта
-        catch(e) //у - содержит значение исключения
-        {
+// создание ajax-объекта с поддержкой браузеров
+function createRequestObject() {
+    //Gecko-совместимые браузеры, Safari, Konqueror
+    try {
+        //создание объекта
+        return new XMLHttpRequest();
+    } catch (e) {
+        // запрос бросает исключение
+        //Internet explorer
+        try {
+            return new ActiveXObject('Msxml2.XMLHTTP')
+        } catch (e) {
             //Internet explorer
-            try { return new ActiveXObject('Msxml2.XMLHTTP') }
-            catch(e)
-            {
-                //Internet explorer
-                try { return new ActiveXObject('Microsoft.XMLHTTP') }
-                catch(e) { return null; }
-           }
+            try { return new ActiveXObject('Microsoft.XMLHTTP') }
+            catch (e) { return null; }
+        }
     }
 }
+//сюда передаем ссылку
+function linkcontent(link, content_selector, loading_text = "loading text") {
+    //Получаем ID контента
+    let contents = document.body.querySelectorAll(content_selector);
+    //получить HTML-содержимое элемента в виде строки "Идет загрузка.."
+    for (let content of contents) {
+        //Создаем объект Ajax
+        content.innerHTML = loading_text;
+        let http = createRequestObject();
+        //если объект создан 
+        if (http) {
+            //Созданный объект инициализирует новый запрос
+            //Параметры: метод GET, ссылка
+            http.open('get', link);
+            //задаётся обработчик, который вызывается при смене статуса объекта
+            http.onreadystatechange = function () {
+                //readyState — число, обозначающее статус объекта
+                //4 - объект инициализирован, получен ответ от сервера
+                if (http.readyState == 4) {
+                    //представление ответа сервера в виде текста
+                    content.innerHTML = http.responseText;
+                }
+            }
+            http.send(null);
+            delete http;
+        }
+        else {
+            document.location = link;
+        }
+    }
 } //end function
